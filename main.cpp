a70a4875346dc5a3d1e8615f228ce7ea2c70fa6e
Bartłomiej Szewczyk <bartek1739@gmail.com>
2019-01-16 03:39:31 +0100

    autostrada a1

--- main.cpp
+++ main.cpp
@@ -13,11 +13,11 @@ int main()
     std::cout<<"Authors: Antoni Messyasz, Barlomiej Szewczyk"<<std::endl;
     std::cout << "Hello Road!" << std::endl;
     std::uniform_real_distribution<double> random_speed(6.0, 25.0);
-    std::uniform_real_distribution<double> random_distance(2.0, 2000.0);
+    std::uniform_real_distribution<double> random_distance(10.0, 2000.0);
     std::default_random_engine re(std::chrono::system_clock::now().time_since_epoch().count());
-#ifdef NO_GRAPHICS
+//#ifdef NO_GRAPHICS
     for (int i = 0; i < 100; ++i) {
-#endif
+//#endif
         //std::random_device r("mt19937");
         double speedA = random_speed(re);
         double speedB = random_speed(re);
@@ -28,7 +28,7 @@ int main()
         //speedA, speedB, speedC, distanceAB, distanceAC, distanceToEnd, rightLane
         Road road(speedA, speedB, speedC, distanceAB, distanceAC, distanceToEnd, right);
         road.simulate();
-#ifdef NO_GRAPHICS
+//#ifdef NO_GRAPHICS
     }
-#endif
+//#endif
 }
